428b4ef240855ffcd24a2fe6e495b4a752cf770e
javierwhj <javierwhj@gmail.com>
2021-08-14 11:03:14 +0800

    Add config changes

--- midas-backend/src/index.ts
+++ midas-backend/src/index.ts
@@ -1,6 +1,8 @@
 import "reflect-metadata";
+require('dotenv').config()
 import { createConnection } from "typeorm";
 import express, { Application } from "express";
+var cors = require('cors')
 import morgan from "morgan";
 import swaggerUi from "swagger-ui-express";
 
@@ -10,6 +12,8 @@ import dbConfig from "./config/database";
 const PORT = process.env.PORT || 8000;
 
 const app: Application = express();
+
+app.use(cors())
 
 app.use(express.json());
 app.use(morgan("tiny"));
